f06fea28b4f0686fa20b7d6343a600a5c454a29e
Marco Oliva <marco.oliva@ufl.edu>
2023-02-20 11:44:17 -0500

    Produce contigs info.

--- mvtool.cpp
+++ mvtool.cpp
@@ -8,36 +8,58 @@
 
 // from pfp
 #include <vcf.hpp>
+#include <pfp_algo.hpp>
 
 // from leviosam
 #include <leviosam.hpp>
 
 void
-serialize_lifting_curr_sample(vcfbwt::Sample& sample, std::size_t sample_genotype, std::size_t length, std::ofstream& out_stream)
+serialize_lifting_curr_sample (
+vcfbwt::Sample& sample,
+std::size_t sample_genotype,
+std::size_t length,
+const std::vector<std::size_t>& reference_lengths,
+const std::vector<std::size_t>& sample_contig_lengths,
+std::ofstream& out_stream)
 {
-    lift::Lift_builder lvs_builder(length);
+    assert(reference_lengths.size() == sample_contig_lengths.size());
     
-    for(size_t v = 0; v < sample.variations.size(); v++)
+    std::size_t var_it = 0;
+    std::size_t offset_ref = 0;
+    std::size_t offset_sample = 0;
+    for (std::size_t i = 0; i < reference_lengths.size(); i++)
     {
-        const vcfbwt::Variation& variation = sample.get_variation(v);
+        std::size_t ref_contig_length = reference_lengths[i];
+        std::size_t sample_contig_length = sample_contig_lengths[i];
+    
+        lift::Lift_builder lvs_builder(length);
         
-        const size_t var_genotype = sample.genotypes[v].at(sample_genotype);
-        
-        // get only variation in the current genotype
-        if( var_genotype == 0) { continue; }
-        
-        int rlen = variation.alt[0].size(); // length of the reference allele
-        int alen = variation.alt[var_genotype].size(); // length of the alternate allele
-        lvs_builder.set(variation.pos, variation.types[var_genotype], rlen, alen );
+        while (sample.get_variation(var_it).pos < offset_ref + ref_contig_length)
+        {
+            const vcfbwt::Variation& variation = sample.get_variation(var_it);
+            const size_t var_genotype = sample.genotypes[var_it].at(sample_genotype);
+    
+            // get only variation in the current genotype
+            if( var_genotype == 0) { continue; }
+    
+            int rlen = variation.alt[0].size(); // length of the reference allele
+            int alen = variation.alt[var_genotype].size(); // length of the alternate allele
+            lvs_builder.set(variation.pos, variation.types[var_genotype], rlen, alen );
+            
+            // keep iterating
+            var_it++;
+        }
+    
+        // build lifting data_structure
+        lift::Lift lift(lvs_builder);
+        out_stream.write((char *)&offset_sample, sizeof(offset_sample));
+    
+        // serialize the data structure
+        lift.serialize(out_stream);
+    
+        offset_ref += ref_contig_length;
+        offset_sample += sample_contig_length;
     }
-    
-    // build lifting data_structure
-    lift::Lift lift(lvs_builder);
-    size_t offset = 0;
-    out_stream.write((char *)&offset, sizeof(offset));
-    
-    // serialize the data structure
-    lift.serialize(out_stream);
 }
 
 int main(int argc, char **argv)
@@ -88,11 +110,25 @@ int main(int argc, char **argv)
     std::ofstream tmp_lifting_out_stream(tmp_lifting_name);
     
     // reference length
-    std::string reference_id = "reference";
-    std::size_t reference_length =  vcf.get_reference().size() + w;
+    std::size_t curr_contig_length = 0;
+    std::size_t contig_id = 1;
+    for (char c : vcf.get_reference())
+    {
+        curr_contig_length++;
+        if (c == vcfbwt::pfp::SPECIAL_TYPES::DOLLAR_PRIME)
+        {
+            names.emplace_back("REF_CONTIG_" + std::to_string(contig_id));
+            lengths.emplace_back(curr_contig_length);
     
-    names.push_back(reference_id);
-    lengths.push_back(reference_length);
+            curr_contig_length = 0;
+            contig_id++;
+        }
+    }
+    // Last contig from reference
+    names.emplace_back("REF_CONTIG_" + std::to_string(contig_id));
+    lengths.emplace_back(curr_contig_length);
+    
+    std::vector<std::size_t> reference_lengths = lengths;
     
     for (std::size_t i = 0; i < vcf.size(); i++)
     {
@@ -102,39 +138,68 @@ int main(int argc, char **argv)
             if (haplotype_string == "1") { sample_genotype = 0; }
             else { sample_genotype = 1; }
             
+            // lengths for each contig
+            std::vector<std::size_t> contig_lengths;
+            curr_contig_length = 0;
+            contig_id = 1;
             vcfbwt::Sample::iterator it(vcf[i], sample_genotype);
             
-            // lengths
-            std::size_t length = it.length() + w;
-            if (i == vcf.size() - 1) { length += w - 1; } // last sample has w dollar primes and a dollar
-            names.push_back(vcf[i].id() + "_H" + haplotype_string);
-            lengths.push_back(length);
+            while (not it.end())
+            {
+                curr_contig_length++;
+                if (*it == vcfbwt::pfp::SPECIAL_TYPES::DOLLAR_PRIME)
+                {
+                    names.emplace_back(vcf[i].id() + "_H" + haplotype_string + "_CONTIG_" + std::to_string(contig_id));
+                    lengths.emplace_back(curr_contig_length);
+                    contig_lengths.emplace_back(curr_contig_length);
+    
+                    curr_contig_length = 0;
+                    contig_id++;
+                }
+                ++it;
+            }
+            // Last contig from current sample
+            names.emplace_back(vcf[i].id() + "_H" + haplotype_string + "_CONTIG_" + std::to_string(contig_id));
+            lengths.emplace_back(curr_contig_length + w); // w - 1 dollar prime and a dollar
+            contig_lengths.emplace_back(curr_contig_length + w);
             
             // lifting
-            serialize_lifting_curr_sample(vcf[i], sample_genotype, length, tmp_lifting_out_stream);
+            serialize_lifting_curr_sample(vcf[i], sample_genotype, it.length(), reference_lengths, contig_lengths, tmp_lifting_out_stream);
         }
         else
         {
-            // first haplotype
-            vcfbwt::Sample::iterator it_h1(vcf[i], 0);
+            for (std::size_t sample_genotype = 0; sample_genotype < 2; sample_genotype++)
+            {
+                std::string haplotype_string = std::to_string(sample_genotype + 1);
+                
+                // lengths for each contig
+                std::vector<std::size_t> contig_lengths;
+                curr_contig_length = 0;
+                contig_id = 1;
+                vcfbwt::Sample::iterator it(vcf[i], sample_genotype);
     
-            // second haplotype
-            vcfbwt::Sample::iterator it_h2(vcf[i], 1);
+                while (not it.end())
+                {
+                    curr_contig_length++;
+                    if (*it == vcfbwt::pfp::SPECIAL_TYPES::DOLLAR_PRIME)
+                    {
+                        names.emplace_back(vcf[i].id() + "_H" + haplotype_string + "_CONTIG_" + std::to_string(contig_id));
+                        lengths.emplace_back(curr_contig_length);
+                        contig_lengths.emplace_back(curr_contig_length);
             
-            // lengths
-            std::size_t length_h1 = it_h1.length() + w;
-            std::size_t length_h2 = it_h2.length() + w;
-            if (i == vcf.size() - 1) { length_h2 += w - 1; } // last sample has w dollar primes and w dollars
+                        curr_contig_length = 0;
+                        contig_id++;
+                    }
+                    ++it;
+                }
+                // Last contig from current sample
+                names.emplace_back(vcf[i].id() + "_H" + haplotype_string + "_CONTIG_" + std::to_string(contig_id));
+                lengths.emplace_back(curr_contig_length + w); // w - 1 dollar prime and a dollar
+                contig_lengths.emplace_back(curr_contig_length + w);
     
-            names.push_back(vcf[i].id() + "_H1");
-            lengths.push_back(length_h1);
-    
-            names.push_back(vcf[i].id() + "_H2");
-            lengths.push_back(length_h2);
-    
-            // lifting
-            serialize_lifting_curr_sample(vcf[i], 0, length_h1, tmp_lifting_out_stream);
-            serialize_lifting_curr_sample(vcf[i], 1, length_h2, tmp_lifting_out_stream);
+                // lifting
+                serialize_lifting_curr_sample(vcf[i], sample_genotype, it.length(), reference_lengths, contig_lengths, tmp_lifting_out_stream);
+            }
         }
     }
     
@@ -149,7 +214,7 @@ int main(int argc, char **argv)
     lengths_out_stream.close();
     
     
-    // Writing out lifitng structure
+    // Writing out lifting structure
     vcfbwt::DiskWrites::update(tmp_lifting_out_stream.tellp());
     tmp_lifting_out_stream.close();
     
@@ -185,20 +250,23 @@ int main(int argc, char **argv)
     }
     
     // total number of contigs
-    std::size_t n_contigs = 1;
+    std::size_t n_contigs = names.size();
     lifting_out_stream.write((char *)&n_contigs, sizeof(n_contigs));
     
-    // empty lifting for the references
-    const size_t len = vcf.get_reference().size() + w;
+    // empty liftings for the references
+    for (std::size_t i = 0; i < reference_lengths.size(); i++)
+    {
+        size_t len = reference_lengths[i];
     
-    sdsl::bit_vector ibv(len);
-    sdsl::bit_vector dbv(len);
-    sdsl::bit_vector sbv(len);
+        sdsl::bit_vector ibv(len);
+        sdsl::bit_vector dbv(len);
+        sdsl::bit_vector sbv(len);
     
-    lift::Lift lift(ibv, dbv, sbv);
+        lift::Lift lift(ibv, dbv, sbv);
     
-    sdsl::serialize(len, lifting_out_stream);
-    lift.serialize(lifting_out_stream);
+        sdsl::serialize(len, lifting_out_stream);
+        lift.serialize(lifting_out_stream);
+    }
     
     // merge tmp liftings from vcf
     std::ifstream tmp_lifting_in(tmp_lifting_name);
